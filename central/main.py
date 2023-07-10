992afe32d113f2e0b627f54ed6b12e253306f192
Yixing <yixingqie@gmail.com>
2023-07-10 13:24:23 -0400

    Added Skeleton for distance thread

--- central/main.py
+++ central/main.py
@@ -11,10 +11,12 @@ if __name__ == "__main__":
     lighting_thread = threading.Thread(target=lighting.update_lights)
     comm_thread = threading.Thread(target=comm.start_comm)
     accel_thread = threading.Thread(target=accel.start_accel)
+    # distance_thread = threading.Thread(target=distance.start_distance)
 
     lighting_thread.start()
     comm_thread.start()
     accel_thread.start()
+    # distance_thread.start()
 
     try:
         while True:
@@ -27,4 +29,6 @@ if __name__ == "__main__":
         lighting_thread.join()
         comm_thread.join()
         accel_thread.join()
+        # distance_thread.join()
+        
         sys.exit()
